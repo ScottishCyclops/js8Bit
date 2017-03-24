093e4709e7c3413255a80c135582b19f9322967f
Scott Winkelmann <scottlandart@gmail.com>
2017-03-24 13:54:32 +0100

    Minor bug fix, added copyright notice

--- drawing.js
+++ drawing.js
@@ -1,3 +1,21 @@
+ /*
+    This is a simple chess game made with P5.js
+    Copyright (C) 2017 Scott Winkelmann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 class Drawing
 {
     constructor(palette,cols,rows,pixelSize,maxUndoSteps)
@@ -22,7 +40,7 @@ class Drawing
 
     drawPixel(x,y)
     {
-        //this.pushUndo();
+        this.pushUndo();
         this.pixels[x+y*this.cols] = this.paintingColor;
     }
 
@@ -61,6 +79,6 @@ class Drawing
 
     redo()
     {
-        this.pixels = this.undoSteps[0];
+        this.pixels = this.undoSteps[undoPosition-1];
     }
 }
