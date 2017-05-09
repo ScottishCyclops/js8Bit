a6ba7fb2ae1372bab60dc34f024158adb5577fa5
Scott Winkelmann <scottlandart@gmail.com>
2017-05-09 11:44:05 +0200

    added copyright and disclaimer

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,23 @@
+ /*
+    This is a simple 8bit painter made with P5.js
+    Copyright (C) 2017 Scott Winkelmann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//THIS FILE IS A COMPLETE WORK IN PROGRESS
+
 const cols = 16;
 const rows = 16;
 const maxColor = cols;
